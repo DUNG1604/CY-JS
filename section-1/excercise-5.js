0b23a7ff64bb2a403f3ec8c93ad5a625621a1c6c
DUNG1604 <dung0373691479@gmail.com>
2024-10-14 15:31:35 +0700

    add

--- section-1/excercise-5.js
+++ section-1/excercise-5.js
@@ -8,10 +8,13 @@ console.log(formatStr);
 
 const str1 = 'Xin 20 c{h}ào 60  #Cy30@!Tech(VN)  100 ';
 // const str1 = ''
-const formatStr1 = str1.replace(/[@#\[\]()!{}]/g, '').split(' ').filter(s => s != '').filter(word => !isNaN(word));
+const formatStr1 = str1.replace(/[@#\[\]()!{}]/g, '')
+    .split(' ')
+    .filter(s => s != '')
+    .filter(word => !isNaN(word));
 const arrNumber = formatStr1.map(Number);
-const sum = arrNumber.reduce((sum, num) => sum + num, 0); 
-const avr = arrNumber.length > 0 ? (sum / arrNumber.length) : 0; 
+const sum = arrNumber.reduce((sum, num) => sum + num, 0);
+const avr = arrNumber.length > 0 ? (sum / arrNumber.length) : 0;
 const formatAvr = avr.toFixed(1);
 
 console.log(formatAvr);
